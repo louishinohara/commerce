faed0ee5474d82703a46a279e695167b1acc1425
Ushio Shinohara <louishinohara@gmail.com>
2025-03-08 14:24:12 -0500

    clean up footer

--- components/layout/footer/FooterBottomSection.tsx
+++ components/layout/footer/FooterBottomSection.tsx
@@ -11,7 +11,7 @@ export default function FooterBottomSection({
   sizeInRem?: number; // ✅ Optional size parameter
 }) {
   return (
-    <div className="border-t border-gray-800 py-4 text-xs text-gray-500">
+    <div className="border-t border-gray-600 py-4 text-xs text-gray-400">
       <div className="mx-auto flex max-w-7xl flex-row items-center justify-between gap-3 px-8 md:px-12 lg:px-16">
         <p className="font-light tracking-tight whitespace-nowrap">
           &copy; {displayYear} {companyName}
